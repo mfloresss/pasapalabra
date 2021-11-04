dedb61893d015738017e063d97108789252af9d2
Michael Flores <mich4el.flores@gmail.com>
2021-11-04 09:27:58 -0300

    Update words

--- js/keyswords.js
+++ js/keyswords.js
@@ -1,203 +1,190 @@
 const words = [
   {
     letra: "A",
-    palabra: "hola",
+    palabra: "ALGORITMO",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: false,
+      "Conjunto de instrucciones o reglas definidas que permiten solucionar un problema",
+    empieza: true,
     contiene: false,
   },
   {
     letra: "B",
-    palabra: "hola",
+    palabra: "BACKEND",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+      "Parte logica de una aplicacion o pagina web que esta conectada con la base de datos",
     empieza: true,
     contiene: false,
   },
   {
     letra: "C",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+    palabra: "CSS",
+    descripcion: "Lenguaje que determina el estilo de los documentos HTML",
     empieza: true,
     contiene: false,
   },
   {
     letra: "D",
-    palabra: "hola",
+    palabra: "DER",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+      "Herramienta de modelado de sistemas, que se concentra en los datos almacenados en el sistema y las relaciones entre éstos",
     empieza: true,
     contiene: false,
   },
   {
     letra: "E",
-    palabra: "hola",
+    palabra: "MER",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+      "Herramienta para el modelo de datos, la cual facilita la representación de entidades de una base de datos.",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "F",
-    palabra: "hola",
+    palabra: "FOREING KEY",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+      "Variable que se utiliza cuando nuestro atributo hace referencia un atributo de otra tabla",
     empieza: true,
     contiene: false,
   },
   {
     letra: "G",
-    palabra: "hola",
+    palabra: "SWING",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+      "Biblioteca en el paquete estándar de Java para la creación de interfaces gráficas de usuario",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "H",
-    palabra: "hola",
+    palabra: "HTML",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+      "Lenguaje de marcado que se utiliza para la elaboracion de paginas web",
     empieza: true,
     contiene: false,
   },
   {
     letra: "I",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+    palabra: "INT",
+    descripcion: "Tipo de dato que permite números enteros",
     empieza: true,
     contiene: false,
   },
   {
     letra: "J",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+    palabra: "JAVASCRIPT",
+    descripcion: "Lenguaje de programación para paginas web",
     empieza: true,
     contiene: false,
   },
   {
     letra: "L",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+    palabra: "VISUAL STUDIO CODE",
+    descripcion: "Editor de texto muy usado por desarrolladores",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "M",
-    palabra: "hola",
+    palabra: "FREECODECAMP",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+      "Plataforma web de enseñanza interactiva accesible para todo el mundo,que se basa en la enseñanza del desarrollo web",
+    empieza: false,
+    contiene: true,
   },
+
   {
     letra: "N",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+    palabra: "Null",
+    descripcion: "Tipo de retorno que indica que algo esta vacio",
     empieza: true,
     contiene: false,
   },
   {
     letra: "Ñ",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+    palabra: "Añadir",
+    descripcion: "Agregar/incorporar algo a otra cosa",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "O",
-    palabra: "hola",
+    palabra: "Bootstrap",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+      "Biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "P",
-    palabra: "hola",
+    palabra: "PRIMARY KEY",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+      "Identifica de manera única cada fila de una tabla de una base de datos",
     empieza: true,
     contiene: false,
   },
   {
     letra: "Q",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
+    palabra: "QUERY",
+    descripcion: "Pregunta o consulta a la base de datos",
+    empieza: false,
     contiene: false,
   },
   {
     letra: "R",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+    palabra: "RELACION",
+    descripcion: "Asociacion entre entidades",
     empieza: true,
     contiene: false,
   },
   {
     letra: "S",
-    palabra: "hola",
+    palabra: "SELECT",
     descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+      "Comando que se utiliza en base de datos para seleccinar datos de una o mas tablas",
     empieza: true,
     contiene: false,
   },
   {
     letra: "T",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
+    palabra: "TABLA",
+    descripcion: "Objetos de base de datos que contienen todos sus datos",
     empieza: true,
     contiene: false,
   },
   {
     letra: "U",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+    palabra: "RETURN",
+    descripcion: "Instruccion que devuelve un valor de una funcion",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "V",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+    palabra: "DIV",
+    descripcion: "Etiqueta de HTML que sirve para agrupar contenidos",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "X",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+    palabra: "HEXADECIMAL",
+    descripcion: "Sistema de numeración que tiene como base el número 16",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "Y",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+    palabra: "Body",
+    descripcion: "Etiqueta HTML que representa el cuerpo de la pagina web",
+    empieza: false,
+    contiene: true,
   },
   {
     letra: "Z",
-    palabra: "hola",
-    descripcion:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae tempore natus dolorum expedita?",
-    empieza: true,
-    contiene: false,
+    palabra: "NORMALIZACION",
+    descripcion: "Proceso de organización de datos en una base de datos",
+    empieza: false,
+    contiene: true,
   },
 ];
 
